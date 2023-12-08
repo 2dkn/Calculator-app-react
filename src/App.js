6cbdd2ba900559e7eb37be27af9794abbde65720
2dkn <2dkneely@gmail.com>
2023-12-08 13:12:47 -0500

    Added the skeleton of app. Need to add scss next

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
+import "./styles/main.scss";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <h3 className="calc">calc</h3>
+        <div>
+          <h1>Theme</h1>
+          <input type="radio"></input>
+          <input type="radio"></input>
+          <input type="radio"></input>
+        </div>
+      </div>
+      <div className="screen"></div>
+      <div className="all-buttons">
+        <button className="calc-buttons" onClick={() => onclick()}>
+          del
+        </button>
+        <button className="calc-buttons" onclick={() => onclick()}>
+          +
+        </button>
+        <button className="calc-buttons" onclick={() => onclick()}>
+          -
+        </button>
+        <button className="calc-buttons" onclick={() => onclick()}>
+          x
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(1)}>
+          1
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(2)}>
+          2
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(3)}>
+          3
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(4)}>
+          4
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(5)}>
+          5
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(6)}>
+          6
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(7)}>
+          7
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(8)}>
+          8
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(9)}>
+          9
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(0)}>
+          0
+        </button>
+        <button className="reset-equal" onclick={() => onclick()}>
+          RESET
+        </button>
+        <button className="reset-equal" onclick={() => onclick()}>
+          =
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
