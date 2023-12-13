be21ffa4bffd3c3fb86d92cb9029d6f1d391c7e9
2dkn <2dkneely@gmail.com>
2023-12-13 11:10:12 -0500

    Finished styling buttons. Now need to style screen and header.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 import "./styles/main.scss";
 
 function App() {
+  // const [displayValue, setDisplayValue] = useState("0");
+
   return (
     <div className="container">
       <div className="header">
@@ -12,56 +15,61 @@ function App() {
       </div>
       <div className="screen"></div>
       <div className="container-btn">
-        <div className="all-buttons">
-          <button className="calc-buttons" onClick={() => onclick()}>
-            del
-          </button>
-          <button className="calc-buttons" onclick={() => onclick()}>
-            +
-          </button>
-          <button className="calc-buttons" onclick={() => onclick()}>
-            -
-          </button>
-          <button className="calc-buttons" onclick={() => onclick()}>
-            x
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(1)}>
-            1
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(2)}>
-            2
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(3)}>
-            3
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(4)}>
-            4
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(5)}>
-            5
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(6)}>
-            6
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(7)}>
-            7
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(8)}>
-            8
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(9)}>
-            9
-          </button>
-          <button className="calc-buttons" onClick={() => onclick(0)}>
-            0
-          </button>
-          <button className="reset-equal" onclick={() => onclick()}>
-            RESET
-          </button>
-          <button className="reset-equal" onclick={() => onclick()}>
-            =
-          </button>
-        </div>
+        <button className="calc-buttons" onClick={() => onclick()}>
+          7
+        </button>
+        <button className="calc-buttons" onclick={() => onclick()}>
+          8
+        </button>
+        <button className="calc-buttons" onclick={() => onclick()}>
+          9
+        </button>
+        <button className="calc-buttons del-button" onclick={() => onclick()}>
+          DEL
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(1)}>
+          4
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(2)}>
+          5
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(3)}>
+          6
+        </button>
+
+        <button className="calc-buttons" onClick={() => onclick(5)}>
+          +
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(6)}>
+          1
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(7)}>
+          2
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(8)}>
+          3
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(9)}>
+          -
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(0)}>
+          .
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(0)}>
+          0
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(0)}>
+          /
+        </button>
+        <button className="calc-buttons" onClick={() => onclick(0)}>
+          x
+        </button>
+        <button className="reset-equal reset-button" onclick={() => onclick()}>
+          RESET
+        </button>
+        <button className="reset-equal equal-button" onclick={() => onclick()}>
+          =
+        </button>
       </div>
     </div>
   );
