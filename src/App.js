16f4685b499edaa4c97da73154ddc7dd4ba3f724
2dkn <2dkneely@gmail.com>
2023-12-22 11:12:50 -0500

    Fixed the screen now it displays button click. Now to solve the logic for each button.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,55 @@ import React, { useState } from "react";
 import * as mathjs from "mathjs";
 import "./styles/main.scss";
 
+const buttonData = [
+  { value: 7, type: "number" },
+  { value: 8, type: "number" },
+  { value: 9, type: "number" },
+  { value: "DEL", type: "del" },
+  { value: 4, type: "number" },
+  { value: 5, type: "number" },
+  { value: 6, type: "number" },
+  { value: "+", type: "operator" },
+  { value: 1, type: "number" },
+  { value: 2, type: "number" },
+  { value: 3, type: "number" },
+  { value: "-", type: "operator" },
+  { value: ".", type: "operator" },
+  { value: 0, type: "number" },
+  { value: "/", type: "operator" },
+  { value: "x", type: "operator" },
+  { value: "RESET", type: "function" },
+  { value: "=", type: "operator" },
+];
+
+function App({ type, onClick, theme, handleClick }) {
+  const [currentTheme, setCurrentTheme] = useState("theme-1");
+  const [calculation, setCalculation] = useState("");
+
+  return (
+    <div className={`container ${currentTheme}`}>
+      <div className={`numbers ${currentTheme}`}>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </div>
+      <div className={`header ${currentTheme}`}>
+        <h3 className={`calc ${currentTheme}`}>calc</h3>
+        <h1 className={currentTheme}>THEME</h1>
+        <ToggleSwitch />
+      </div>
+      <div className={`screen ${currentTheme}`}>
+        <div className="calc-numbers">{calculation || "0"}</div>
+      </div>
+      <CalculatorButtons
+        theme={currentTheme}
+        calculation={calculation}
+        setCalculation={setCalculation}
+      />
+    </div>
+  );
+}
+
 function ToggleSwitch() {
   const [selectedState, setSelectedState] = useState(1);
 
@@ -49,27 +98,7 @@ function ToggleSwitch() {
   );
 }
 
-const buttonData = [
-  { value: 7, type: "number" },
-  { value: 8, type: "number" },
-  { value: 9, type: "number" },
-  { value: "DEL", type: "del" },
-  { value: 4, type: "number" },
-  { value: 5, type: "number" },
-  { value: 6, type: "number" },
-  { value: "+", type: "operator" },
-  { value: 1, type: "number" },
-  { value: 2, type: "number" },
-  { value: 3, type: "number" },
-  { value: "-", type: "operator" },
-  { value: ".", type: "operator" },
-  { value: 0, type: "number" },
-  { value: "/", type: "operator" },
-  { value: "x", type: "operator" },
-  { value: "RESET", type: "function" },
-  { value: "=", type: "operator" },
-];
-
+// Color and size for buttons, del, equal and reset
 const Button = ({ value, type, onClick, theme }) => (
   <button
     className={`calc-buttons ${theme} 
@@ -87,102 +116,25 @@ const Button = ({ value, type, onClick, theme }) => (
   </button>
 );
 
-// const CalculatorButtons = ({ value, type, theme, onClick }) => (
-
-//   <div className={`container-btn ${theme}`}>
-//     {buttonData.map((item) => (
-//       <Button
-//         key={item.value}
-//         value={item.value}
-//         type={item.type}
-//         onClick={() => onClick(item.value)}
-//         theme={theme}
-//       />
-//     ))}
-//   </div>
-// );
-
-const CalculatorButtons = ({ theme }) => {
+const CalculatorButtons = ({ theme, value, calculation, setCalculation }) => {
   // Define handleClick here
   const handleClick = (value) => {
-    // Button click logic goes here
+    setCalculation(value);
   };
 
   return (
     <div className={`container-btn ${theme}`}>
-      {buttonData.map((item) => (
+      {buttonData.map((item, i) => (
         <Button
-          key={item.value}
+          key={i}
           value={item.value}
           type={item.type}
-          onClick={() => handleClick(item.value)} // Use the local handleClick
+          onClick={() => handleClick(item.value)}
           theme={theme}
         />
       ))}
     </div>
   );
 };
-
-function App({ value, type, onClick, theme }) {
-  const [currentTheme, setCurrentTheme] = useState("theme-1");
-  const [calculation, setCalculation] = useState("");
-
-  const handleClick = (value) => {
-    switch (value) {
-      case "DEL":
-        setCalculation({
-          calculation: calculation.slice(0, -1), // Remove last character
-        });
-        break;
-      case "+":
-      case "-":
-      case "/":
-      case "x":
-      case ".":
-        setCalculation({
-          calculation: calculation + value, // Append operator/decimal
-        });
-        break;
-      case "RESET":
-        setCalculation({
-          calculation: "", // Clear calculation. means empty string
-        });
-        break;
-      case "=":
-        // Perform calculation using a library like mathjs
-        const result = mathjs.evaluate(calculation);
-        setCalculation({
-          calculation: result.toString(), // Display result
-        });
-        break;
-      default:
-        // Handle numbers
-        setCalculation({
-          calculation:
-            calculation.length === 0 ||
-            calculation[calculation.length - 1] === "="
-              ? value // Start a new number
-              : calculation + value, // Append to existing number
-        });
-    }
-  };
-
-  return (
-    <div className={`container ${currentTheme}`}>
-      <div className={`numbers ${currentTheme}`}>
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-      </div>
-      <div className={`header ${currentTheme}`}>
-        <h3 className={`calc ${currentTheme}`}>calc</h3>
-        <h1 className={currentTheme}>THEME</h1>
-        <ToggleSwitch />
-      </div>
-      <div className={`screen ${currentTheme}`}>{calculation}</div>
-      <CalculatorButtons theme={currentTheme} onClick={handleClick} />
-    </div>
-  );
-}
 
 export default App;
